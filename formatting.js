9a4ede8238afd426c0c9bad103399cee21c1aa49
Bru <annabru.abh@gmail.com>
2019-06-25 13:10:29 +0200

    Finished up to 2a.

---
+++ formatting.js
@@ -0,0 +1,11 @@
+const removeWhitespace = input => {
+  // Trim whitespace from the beginning and end of the param
+  let result = input.trim();
+
+  //collapse duplicate spaces - using regex
+  result = result.replace(/\s+/g, " ");
+
+  console.log(result);
+};
+
+removeWhitespace("  sao tome     ");
