65512cac1394c5e561d376aa3aa07eaf4bdcccb7
Bru <annabru.abh@gmail.com>
2019-07-03 13:17:40 +0200

    Changed client.js to a simpler axios API call request.

--- client.js
+++ client.js
@@ -1,60 +1,10 @@
-const { version, name } = require("./package.json");
-const { prepareString } = require("./formatting");
-
 const axios = require("axios");
 
-const getTimeData = async (area, city) => {
-  const response = await axios(
-    `http://worldtimeapi.org/api/timezone/${
-      area && city ? area + "/" + city : ""
-    }`
-  );
+// const area = process.argv[2];
+// const city = process.argv[3];
+// const url = ;
 
-  return response.data;
-};
-
-//this was on index.js in the other repository, together with require()
-
-const arguments = process.argv.slice(2);
-
-if (arguments.includes("--help")) {
-  console.log(`
-Thanks for using ${name} 🎉
-version: ${version}
-You need to provide at least two arguments your area (continent) and a valid city.
-Use --options to see available arguments.
-  `);
-  process.exit();
-}
-
-if (arguments.includes("--option")) {
-  getTimeData()
-    .then(data => {
-      return data.reduce((acc, curr) => {
-        const [area, city] = curr.split("/");
-        
-        acc.push({ area, city });
-        return acc;
-      }, []);
-    })
-    .then(data => {
-      data.forEach(entry => {
-        console.log(entry);
-      });
-      process.exit();
-    });
-}
-
-getTimeData(arguments[0], arguments[1]).then(data => {
-  switch (arguments[2]) {
-    case "--unix":
-      return console.log(data["unixtime"]);
-    case "--currentWeek":
-      return console.log(data["week_number"]);
-    case "--dayOfTheYear":
-      return console.log(data["day_of_year"]);
-    default:
-      return console.log(data["utc_datetime"]);
-  }
-  process.exit();
-});
+exports.getTimeData = (area, city) =>
+  axios
+    .get(`http://worldtimeapi.org/api/timezone/${area}/${city}`)
+    .then(response => console.log(response.data.utc_datetime));
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Load external resources
-const { prepareString } = require('./formatting');
-const { showHelp } = require('./messaging');
+const { prepareString } = require("./formatting");
+const { showHelp } = require("./messaging");
+const { getTimeData } = require("./client");
 
 // Handle input
-let result = '';
+let area = " ";
+let city = " ";
 const args = process.argv.slice(2);
 
-if (args.includes('--help')) {
-    showHelp();
+if (args.includes("--help")) {
+  showHelp();
 }
 
 // input goes to formatting.js then gets returned back here
@@ -15,8 +17,8 @@ result = prepareString(args[0]);
 
 // just testing
 
-console.log(prepareString('kuala  lumpur'));
-console.log(prepareString('  puerto rico'));
-console.log(prepareString(' tokyo'));
+console.log(prepareString("kuala  lumpur"));
+console.log(prepareString("  puerto rico"));
+console.log(prepareString(" tokyo"));
 
 console.log(result);
